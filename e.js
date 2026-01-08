3b05b42f6e50b71ccfd2b402ae30a61a7b392800
Ding <openpgp@dingthemself.com>
2026-01-08 17:42:45 -0500

    information session!

---
+++ e.js
@@ -0,0 +1,6 @@
+var d = "barnard.edu";
+      var y = ".";
+      var k = "dding";
+      var e = " ";
+      var s = "@";
+      document.write("<a href='" + "mail" + "to:" + k + s + d + "'>" + k + s + d + "</a>");
